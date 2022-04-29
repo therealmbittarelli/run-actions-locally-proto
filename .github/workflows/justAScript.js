791822f9eef186611789b50751794718a3b2fd6f
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2022-04-29 09:44:15 -0400

    add some dwight quotes because obviously

--- .github/workflows/justAScript.js
+++ .github/workflows/justAScript.js
@@ -4,7 +4,13 @@ const quotes = [
     "And I knew exactly what to do. But in a much more real sense, I had no idea what to do.\n- Michael Scott",
     "I wanna do a cartwheel. But real casual-like. Not enough to make a big deal out of it, but I know everyone saw it. One stunning, gorgeous cartwheel.\n- Creed Bratton",
     "If I can’t scuba, then what’s this all been about? What am I working toward?\n- Creed Bratton",
-    "Actually, I didn’t think it was appropriate to invite children, since it’s uh, you know, there’s gambling and alcohol. It’s in our dangerous warehouse, it’s a school night, and you know, Hooter’s is catering, and is that — is that enough? Should I keep going? \n- Toby Flenderson \nWhy are you the way that you are? Honestly, every time I try to do something fun, or exciting, you make it… not that way. I hate… so much about the things that you choose to be.\n- Michael Scott"
+    "Actually, I didn’t think it was appropriate to invite children, since it’s uh, you know, there’s gambling and alcohol. It’s in our dangerous warehouse, it’s a school night, and you know, Hooter’s is catering, and is that — is that enough? Should I keep going? \n- Toby Flenderson \nWhy are you the way that you are? Honestly, every time I try to do something fun, or exciting, you make it… not that way. I hate… so much about the things that you choose to be.\n- Michael Scott",
+    "When Pam gets Michael’s old chair, I get Pam’s old chair. Then I’ll have two chairs. Only one to go.\n- Creed Bratton",
+    "Before I do anything I ask myself, ‘Would an idiot do that?’ And if the answer is yes, I do not do that thing.\n- Dwight Schrute",
+    "People underestimate the power of nostalgia. Nostalgia is truly one of the greatest human weaknesses, second only to the neck.\n- Dwight Schrute",
+    "When my mother was pregnant with me, they did an ultrasound and found she was having twins. When they did another ultrasound a few weeks later, they discovered that I had adsorbed the other fetus. Do I regret this? No, I believe his tissue has made me stronger. I now have the strength of a grown man and a little baby.\n- Dwight Schrute",
+    "In an ideal world, I would have all 10 fingers on my left hand so my right hand could just be a fist for punching.\n- Dwight Schrute",
+    "Dolphins get a lot of good publicity for the drowning swimmers they push back to shore, but what you don’t hear about is the many people they push farther out to sea! Dolphins aren’t smart. They just like pushing things.\n- Dwight Schrute",
 ];
 
 const arrayIndex = Math.floor(Math.random() * quotes.length);
